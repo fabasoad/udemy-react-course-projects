48e5481229855d2734ba51a15ebd66071d565525
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-10-30 17:02:31 +0900

    Passing lesson 82

---
+++ portfolio-site/src/components/ContactPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ContanctPage = () => (
+    <div>
+        <h1>Contact Me</h1>
+        <p>You can contact me by test@gmail.com</p>
+    </div>
+);
+
+export default ContanctPage;
