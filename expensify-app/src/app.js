2a6e476ab09a5188c318afb5d7a2d846533c162a
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2019-11-02 20:42:09 +0900

    Passing lesson 101

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
+import uuid from 'uuid';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+store.subscribe(() => {
+    const state = store.getState();
+    console.log(getVisibleExpenses(state.expenses, state.filters));
+});
+store.dispatch(addExpense({ description: 'Water bill', amount: 5000 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 5000 }));
+store.dispatch(setTextFilter('ter'));
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
